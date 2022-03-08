e8951f754269659d7ecb93cfa2c4cdf2a1d89eea
ArpanCitrusbug <arpan.citrusbug@gmail.com>
2022-03-08 17:52:09 +0530

    Added Functionalities

--- Application/urls.py
+++ Application/urls.py
@@ -1,14 +1,18 @@
 from unicodedata import name
 from django.urls import path
-from Application.views import AddBlog, DetailBlog, IndexView, LogIn, MainBody, Profile
+from Application.models import Category
+from Application.views import AddBlog, DetailBlog, DeletePost, IndexView, LogIn, MainBody, Profile, Logout, Category
 
 from . import views
 
 urlpatterns = [
     path('', IndexView.as_view(), name="Home"),
-    path('login', LogIn.as_view(), name="Login"),
-    path('home', MainBody.as_view(), name="Main Body"),
+    path('login', LogIn.as_view(), name="login"),
+    path('home', MainBody.as_view(), name="MainBody"),
     path('detail/<int:id>',DetailBlog.as_view(), name="Detailed Blog"),
     path('addblog', AddBlog.as_view(), name="Add Blog"),
-    path('profile/<str:username>', Profile.as_view(), name="Profile")
+    path('profile/<str:username>', Profile.as_view(), name="Profile"),
+    path('logout', Logout.as_view(), name="Logout"),
+    # path('category', Category.as_view(), name="Category")
+    path('blog/<int:pk>/delete',DeletePost.as_view(), name= "deleteblog")
 ]
--- Application/views.py
+++ Application/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from django.views.generic import View
-from .models import Post
+import re
+from turtle import title
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.hashers import check_password,make_password
+from django.views.generic import View,DeleteView
+from .models import Post, Category
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -12,15 +16,33 @@ class LogIn(View):
     def get(self, request):
         return render(request, 'login.html')
 
+    def post(self, request):
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=User.objects.get(username=username)
+        print(user.password)
+        password_check= check_password(password,user.password)
+        if password_check:
+            auth.login(request, user)
+            return redirect('MainBody')
+        return render(request, 'login.html')
+
 class MainBody(View):
     def get(self, request):
+        # if request.user.is_anticipated():
+                
         post=Post.objects.all()
+        category=Category.objects.all()
         #sending all the blogs with all function
         context= {
-            'post':post,
-        }
-        #this dictionary will send all the data to template
+                'post':post,
+                'category':category,
+            }
+            #this dictionary will send all the data to template
         return render(request, 'mainbody.html',context)
+
+        # else:
+        return redirect('login')
 
 
 class DetailBlog(View):
@@ -36,8 +58,14 @@ class AddBlog(View):
     def get(self, request):
         return render(request, 'addblog.html')
 
-    def post(self,request):
-        return render(request, 'addblog.html')
+    def post(request):
+        category = Category.objects.all()
+        if request.user.is_authenticated:
+            if request.method == "POST":
+                if request.user.is_authenticated:
+                    title = request.POST.get('title')
+                    content = request.POST.get()
+
 
 
 class Profile(View):
@@ -48,3 +76,22 @@ class Profile(View):
         }
         return render(request, 'profile.html',context)
 
+class Logout(View):
+    def get(self, request):
+        auth.logout(request)
+        return redirect('login')
+
+
+# class Category(View):
+#     def get(self, request):
+#         return render(request, 'category.html')
+
+
+class Signup(View):
+    def get(self,request):
+        pass
+
+
+class DeletePost(DeleteView):
+    model = Post
+    success_url = '/blog/home'
