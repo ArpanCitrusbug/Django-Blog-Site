257661b55890b7596bed2e82c7aa4215aa00ea22
ArpanCitrusbug <arpan.citrusbug@gmail.com>
2022-03-09 18:02:58 +0530

    added functionalities and made page signup page dynamic

--- Application/urls.py
+++ Application/urls.py
@@ -1,18 +1,19 @@
 from unicodedata import name
 from django.urls import path
 from Application.models import Category
-from Application.views import AddBlog, DetailBlog, DeletePost, IndexView, LogIn, MainBody, Profile, Logout, Category
+from Application.views import * 
 
 from . import views
 
 urlpatterns = [
-    path('', IndexView.as_view(), name="Home"),
+    path('', IndexView.as_view(), name="home"),
     path('login', LogIn.as_view(), name="login"),
     path('home', MainBody.as_view(), name="MainBody"),
     path('detail/<int:id>',DetailBlog.as_view(), name="Detailed Blog"),
     path('addblog', AddBlog.as_view(), name="Add Blog"),
     path('profile/<str:username>', Profile.as_view(), name="Profile"),
     path('logout', Logout.as_view(), name="Logout"),
+    path('signup',Signup.as_view(), name="Signup")
     # path('category', Category.as_view(), name="Category")
-    path('blog/<int:pk>/delete',DeletePost.as_view(), name= "deleteblog")
+    # path('blog/<int:pk>/delete',DeletePost.as_view(), name= "deleteblog")
 ]
--- Application/views.py
+++ Application/views.py
@@ -1,16 +1,48 @@
-import re
+
 from turtle import title
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib import auth
 from django.contrib.auth.hashers import check_password,make_password
-from django.views.generic import View,DeleteView
+from django.views.generic import View
 from .models import Post, Category
 from django.contrib.auth.models import User
 
 # Create your views here.
 class IndexView(View):
     def get(self, request):
-        return render(request, 'index.html')
+        # if request.user.is_authenticated():
+            return render(request, 'index.html')
+        # else:
+            # return redirect('login')
+
+
+    def post(self,request):
+        firstname=request.POST['firstname']
+        lastname=request.POST['lastname']
+        username=request.POST['username']
+        password1=request.POST['password1']
+        email=request.POST['email']
+        password2=request.POST['password2']
+        if len(firstname)!=0 and len(lastname)!=0 and len(username)!=0 and len(password1)!=0 and len(email)!=0 and len(password2)!=0:
+            if not User.objects.filter(username=username).exists():
+                if password1 == password2:
+                    user = User.objects.create(first_name=firstname,last_name=lastname,username=username,password=make_password(password1),email=email)
+                    user.save()
+                    return redirect('MainBody')
+                else:
+                    messages.error(request, "Password Doesn't Match")
+            else:
+                messages.info(request, "Username is already taken")
+        else:
+            messages.info(request, "All fields are Required")
+        return render(request,'index.html')
+                # user = User.objects.create(username=username, password=password1, email=email, first_name=firstname, last_name=lastname )
+                # user.save()
+                # print('user created')
+            
+        
+    
 
 class LogIn(View):
     def get(self, request):
@@ -29,34 +61,33 @@ class LogIn(View):
 
 class MainBody(View):
     def get(self, request):
-        # if request.user.is_anticipated():
-                
-        post=Post.objects.all()
-        category=Category.objects.all()
-        #sending all the blogs with all function
-        context= {
-                'post':post,
-                'category':category,
+        if request.user.is_authenticated:
+            blog=Post.objects.all()
+            context= {
+                'post':blog,
             }
-            #this dictionary will send all the data to template
-        return render(request, 'mainbody.html',context)
-
-        # else:
-        return redirect('login')
+            return render(request, 'mainbody.html', context)
+        else:
+            return redirect('login')
 
 
 class DetailBlog(View):
     def get(self, request, id):
-        blog=Post.objects.get(id=id)
-        context= {
+        if request.user.is_authenticated:
+            blog=Post.objects.get(id=id)
+            context= {
             'post':blog,
-        }
-        return render(request, 'detailedblog.html', context)
-
+            }
+            return render(request, 'detailedblog.html', context)
+        else:
+            return redirect('login')
 
 class AddBlog(View):
     def get(self, request):
-        return render(request, 'addblog.html')
+        if request.user.is_authenticated:
+            return render(request, 'addblog.html')
+        else:
+            return redirect('login')
 
     def post(request):
         category = Category.objects.all()
@@ -69,12 +100,16 @@ class AddBlog(View):
 
 
 class Profile(View):
+    
     def get(self, request, username):
-        user=User.objects.get(username=username)
-        context={
+        if request.user.is_authenticated:
+            user=User.objects.get(username=username)
+            context={
             'user':user,
         }
-        return render(request, 'profile.html',context)
+            return render(request, 'profile.html',context)
+        else:
+            return redirect('login')
 
 class Logout(View):
     def get(self, request):
@@ -88,10 +123,12 @@ class Logout(View):
 
 
 class Signup(View):
-    def get(self,request):
-        pass
+    def get(self, request):
+        return render(request, 'index.html')
 
 
-class DeletePost(DeleteView):
-    model = Post
-    success_url = '/blog/home'
+
+
+# class DeletePost(DeleteView):
+#     model = Post
+#     success_url = '/blog/home'
