25b3401a414e340997939d498d6747aefa63cd15
ArpanCitrusbug <arpan.citrusbug@gmail.com>
2022-03-14 17:51:05 +0530

    Added Validation to SignUp page

--- Application/admin.py
+++ Application/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Category, Post, Profile
-
+from .models import Category, Post,Profile
 # Register your models here.
 
 admin.site.register(Category)
