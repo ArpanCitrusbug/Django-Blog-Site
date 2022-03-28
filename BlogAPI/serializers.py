112776cad869b15810dd0292f40f100825b6a073
ArpanCitrusbug <arpan.citrusbug@gmail.com>
2022-03-28 17:41:47 +0530

    serializer class

---
+++ BlogAPI/serializers.py
@@ -0,0 +1,31 @@
+from Application.models import *
+from rest_framework import serializers
+
+class PostSerializers(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+    user = serializers.CharField(read_only=True)
+    class Meta:
+        model = Post
+        fields=['title','post_image','content','category',"id","user"]
+
+
+
+
+class UserSerializers(serializers.Serializer):
+    first_name = serializers.CharField(max_length=20)
+    last_name = serializers.CharField(max_length=20)
+    username = serializers.CharField(max_length=20)
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+    id = serializers.CharField(read_only=True)
+
+
+
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(read_only=True)
+    id = serializers.CharField(read_only=True)
+    class Meta:
+        model = Profile
+        fields = ['user','id']
