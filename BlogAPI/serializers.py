55a2b86b6da458c5ddd85b53af2b9e2bfc01c383
ArpanCitrusbug <arpan.citrusbug@gmail.com>
2022-03-28 18:02:32 +0530

    Serializer updated

--- BlogAPI/serializers.py
+++ BlogAPI/serializers.py
@@ -1,23 +1,37 @@
+from multiprocessing import managers
+from pyexpat import model
 from Application.models import *
 from rest_framework import serializers
 
-class PostSerializers(serializers.ModelSerializer):
-    id = serializers.CharField(read_only=True)
-    user = serializers.CharField(read_only=True)
-    class Meta:
-        model = Post
-        fields=['title','post_image','content','category',"id","user"]
+'''
+[
+    {
+       user:{
 
+       } 
+    }
+]
+'''
 
-
-
-class UserSerializers(serializers.Serializer):
+class UserSerializers(serializers.ModelSerializer):
     first_name = serializers.CharField(max_length=20)
     last_name = serializers.CharField(max_length=20)
     username = serializers.CharField(max_length=20)
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
     id = serializers.CharField(read_only=True)
+    class Meta:
+        model = User
+        fields = ['first_name','last_name','username','email','password','id'] 
+
+
+class PostSerializers(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+    user = UserSerializers()
+    class Meta:
+        model = Post
+        fields=['title','post_image','content','category',"id","user"]
+
 
 
 
